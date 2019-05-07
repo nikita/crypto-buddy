f440775f3513c4518340967cf45b81975d9396e4
никита <23018519+nikita@users.noreply.github.com>
2019-05-07 11:29:10 -0700

    Add purge command to purge messages.

--- index.js
+++ index.js
@@ -203,6 +203,27 @@ client.on("message", async message => {
       await message.channel.send("No current alerts set.");
     }
   }
+
+  if (command === "purge") {
+    // This command removes all messages from all users in the channel, up to 100.
+
+    // get the delete count, as an actual number.
+    const deleteCount = parseInt(args[0], 10);
+
+    // Ooooh nice, combined conditions. <3
+    if (!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply(
+        "Please provide a number between 2 and 100 for the number of messages to delete"
+      );
+
+    // So we get our messages, and delete them. Simple enough, right?
+    const fetched = await message.channel.fetchMessages({ limit: deleteCount });
+    message.channel
+      .bulkDelete(fetched)
+      .catch(error =>
+        message.reply(`Couldn't delete messages because of: ${error}`)
+      );
+  }
 });
 
 client.login(DISCORD_BOT_TOKEN);
